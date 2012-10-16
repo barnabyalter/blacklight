54c0974dbae569233caef20703e184a456cf7a8a
Chris Beer <chris@cbeer.info>
2012-10-16 14:50:58 -0700

    fix facet test mocking

--- test_support/spec/views/catalog/_facets.html.erb_spec.rb
+++ test_support/spec/views/catalog/_facets.html.erb_spec.rb
@@ -5,7 +5,23 @@ describe "catalog/_facets" do
     @mock_config = Blacklight::Configuration.new
     view.stub(:blacklight_config => @mock_config)
   end
+  it "should not have a header if no facets are displayed" do
+    view.stub(:render_facet_partials => '')
+    render
+    rendered.should_not have_selector('h4')
+  end
+
   it "should have a header" do
+
+      @mock_field_1 = mock(:field => 'facet_field_1',
+                       :label => 'label')
+      @mock_display_facet_1 = mock(:name => 'facet_field_1', :items => [mock(:value => 'Value', :hits => 1234)])
+      view.stub(:facet_field_names => [:facet_field_1],
+                :facet_limit_for => 10 )
+
+      @response = mock()
+      @response.stub(:facet_by_field_name).with(:facet_field_1) { @mock_display_facet_1 }
+
     view.stub(:render_facet_partials => '')
     render
     rendered.should have_selector('h4')
@@ -20,7 +36,7 @@ describe "catalog/_facets" do
                 :facet_limit_for => 10 )
 
       @response = mock()
-      @response.should_receive(:facet_by_field_name).with(:facet_field_1) { @mock_display_facet_1 }
+      @response.stub(:facet_by_field_name).with(:facet_field_1) { @mock_display_facet_1 }
 
     end 
 
